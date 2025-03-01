4616cce0cfb911f1ceba7f2eb8f82bd125f8267d
aastikdas <aastikdas126@gmail.com>
2025-03-01 22:40:32 +0530

    Improved styling

--- src/App.jsx
+++ src/App.jsx
@@ -74,46 +74,46 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className='container w-[90vw] sm:w-[80vw]  mx-auto my-5 rounded-xl px-5 py-2 bg-violet-200 h-[80vh] overflow-y-scroll'>
-        <div className="addTodo">
-          <h2 className='text-lg font-bold text-center'>Add a Todo</h2>
-        </div>
-        <div className="input_wala flex justify-center items-center">
-          <input onChange={handleChange} onKeyDown={(event) => event.key == "Enter" && handleAdd()} value={todo} type="text" className='w-[80%] bg-yellow-50 mx-2 rounded-lg px-3 py-1 min-h-10 active:border' />
-          <button onClick={handleAdd} disabled={todo.length < 3} className="btn-primary-save">Save</button>
+      <div className='container mx-1 w-[97vw] sm:w-[80vw]  sm:mx-auto sm:my-5 rounded-xl sm:px-5 sm:py-2 h-[80vh] overflow-y-scroll overflow-x-hidden  bg-[#ffe8d6] border border-red-100'>
+        <div className="div  bg-violet-200 rounded-2xl pb-3">
+          <div className="addTodo">
+            <h2 className='text-lg font-bold text-center'>Add a Todo</h2>
+          </div>
+          <div className="input_wala flex justify-center items-center">
+            <input onChange={handleChange} onKeyDown={(event) => event.key == "Enter" && handleAdd()} value={todo} type="text" className='w-[80%] bg-yellow-50 mx-2 rounded-lg px-3 py-1 min-h-10 active:border' />
+            <button onClick={handleAdd} disabled={todo.length < 3} className="btn-primary-save">Save</button>
 
+          </div>
         </div>
         <input type="checkbox" checked={showFinished} onChange={toggleFinished} name="" id="" /> Show only Finished
         <h2 className='text-lg font-bold text-center'>Your todos</h2>
-        <div className="todos flex flex-col justify-center items-center ">
+        <div className="todos flex flex-col justify-center items-center w-full  rounded-2xl ">
 
           {todos.length === 0 && <div> No todos to display</div>}
           {todos.map(item => {
-            return (showFinished || !item.isCompleted) && <div key={item.id} className="todo flex justify-center items-center ">
-              <div className='flex gap-5 '>
+            return (showFinished || !item.isCompleted) && <div key={item.id} className="todo flex justify-center items-center gap-2 ">
+              <div className=" relative content flex justify-around items-center w-[100vw] sm:w-[80vw]">
+                <div className='flex gap-0.5 sm:gap-5 w-[60vw] sm:w-[40vw] '>
                   <input onChange={handleCheckbox} type="checkbox" checked={item.isCompleted} name={item.id} id="" />
-                  <div className={` ${item.isCompleted ? "line-through opacity-50" : ""} text w-[30vw] bg-yellow-50 m-2 rounded-lg px-3 py-1 min-h-10 flex items-center`}>
+                  <div className={` ${item.isCompleted ? "line-through opacity-50" : ""} text-sm sm:text-lg w-[60vw] sm:w-[80vw] bg-[#ddbea9] m-2 rounded-lg px-3 py-1 min-h-10 flex items-center`}>
                     {item.todo}
                   </div>
-              </div>
-              <div className="buttons w-[90%] ">
-                <button onClick={(e) => { handleEdit(e, item.id) }} className="relative duration-1000 btn-primary-edit w-12 sm:w-20 hover:bg-green-400 ">
-                <span class="sm:hidden"><img src="svg/edit.svg" alt="" srcset="" /></span>
-                <span class="sm:inline hidden">Edit</span>
-                </button>
-                <button onClick={(e) => { handleDelete(e, item.id) }} className="btn-primary-delete w-12 sm:w-20">
-                <span class="sm:hidden"><img src="svg/delete.svg" alt="" srcset="" /></span>
-                <span class="sm:inline hidden">Delete</span>
-                </button>
+                </div>
+                <div className="flex w-30 ">
+                  <button onClick={(e) => { handleEdit(e, item.id) }} className="relative duration-1000 btn-primary-edit w-8 flex justify-center sm:w-20 hover:bg-green-400 ">
+                    <span class="sm:hidden "><img src="svg/edit.svg" alt="" srcset="" /></span>
+                    <span class="sm:inline hidden">Edit</span>
+                  </button>
+                  <button onClick={(e) => { handleDelete(e, item.id) }} className="btn-primary-delete w-8 flex justify-center sm:w-20">
+                    <span className="sm:hidden"><img src="svg/delete.svg" alt="" srcset="" /></span>
+                    <span className="sm:inline hidden">Delete</span>
+                  </button>
+                </div>
               </div>
             </div>
           })}
 
         </div>
-
-
-
-
       </div>
     </>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='bg-slate-900 text-white p-2 mb-2 flex justify-evenly h-20 items-center w-full'>
-      <div className="logo text-lg mx-4 flex gap-2 items-center">
+    <nav className='bg-slate-900 text-white p-0 text-[12px] sm:text-lg sm:p-2 mb-2 flex justify-between h-12 sm:h-20 items-center w-[100vw] '>
+      <div className="logo  mx-1 sm:mx-4 flex gap-2 items-center">
         <div className="icon">
-          <img className='w-12 h-12 rounded-full  hover:cursor-pointer' src="/icon.svg" alt="Profile photo" />
+          <img className='sm:w-12 sm:h-12 rounded-full  hover:cursor-pointer' src="/icon.svg" alt="Profile photo" />
         </div>
-        <div className="text font-bold">
+        <div className="text font-semibold sm:font-bold">
           my_todos
         </div>
       </div>
-      <div className="rightside w-[25vw] flex justify-between mx-4">
-        <span className=' hover:cursor-pointer transition-all ease-in duration-200 hover:border hover:rounded-xl p-2 hover:border-yellow-200 hover:bg-slate-800'>Home</span>
-        <span className=' hover:cursor-pointer transition-all ease-in duration-200 hover:border hover:rounded-xl p-2 hover:border-yellow-200 hover:bg-slate-800'>About</span>
-        <span className=' hover:cursor-pointer transition-all ease-in duration-200 hover:border hover:rounded-xl p-2 hover:border-yellow-200 hover:bg-slate-800'>Help</span>
+      <div className="rightside  flex justify-between mx-1 sm:mx-4 gap-1">
+        <span className=' hover:cursor-pointer transition-all ease-in duration-200 hover:border hover:rounded-xl p-1 sm:p-2 hover:border-yellow-200 hover:bg-slate-800'>Home</span>
+        <span className=' hover:cursor-pointer transition-all ease-in duration-200 hover:border hover:rounded-xl p-1 sm:p-2 hover:border-yellow-200 hover:bg-slate-800'>About</span>
+        <span className=' hover:cursor-pointer transition-all ease-in duration-200 hover:border hover:rounded-xl p-1  sm:p-2 hover:border-yellow-200 hover:bg-slate-800'>Help</span>
       </div>
     </nav>
   )
